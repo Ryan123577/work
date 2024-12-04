3c984e07b68fb16478ddd4b643a1e2fcce3803e9
Ryan123577 <ryanobiudo@gmail.com>
2024-12-04 16:33:43 +0100

    Add files via upload

---
+++ Ryan .js
@@ -0,0 +1,15 @@
+let person = {
+    "Full name": "Obiudo Ryan Chiagozie",
+    Age: 19,
+    "Application ID": "FT240056",
+    "Matric No": "N/A",
+    School: "Computing",
+    Course: "Software Engineering",
+    "Phone No": +2349042805162,
+    Email: "ryanobiudo@gmail.com",
+    Nationality: "Nigerian",
+    Address: "2B HRC Estate, V.G.C , Lagos State"
+    State of Origin: "Anambra State"
+    
+}
+console.log(person)
